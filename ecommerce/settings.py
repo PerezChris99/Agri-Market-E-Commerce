6a87576f6e40a794647d7accaf6afb5ad72e1570
perezchris3599 <kweeziperez712@gmail.com>
2023-04-26 20:15:54 +0300

    hello world

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #our apps
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
+
+def index(request:HttpRequest):
+    return HttpResponse("Hello world")
+
+    
