519c69f79157f25dd2ffca5c99c9cecbd2451aa9
PerezChris99 <kweeziperez712@gmail.com>
2023-05-11 21:56:09 +0300

    ecomm

--- store/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse
-
-# Create your views here.
-
-def index(request:HttpRequest):
-    return HttpResponse("Hello world")
-
-    
