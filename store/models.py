86d518a7999493ecd24193082ebf709594ba9014
PerezChris99 <kweeziperez712@gmail.com>
2023-05-23 14:31:47 +0300

    image error removed

--- store/models.py
+++ store/models.py
@@ -19,6 +19,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
