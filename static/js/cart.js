285ddc18e9dcbaa9c46bcbe4f0a09497d0026b1a
PerezChris99 <kweeziperez712@gmail.com>
2023-05-26 18:14:40 +0300

    user and anonymous user working

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,5 +5,12 @@ for (i = 0; i < updateBtns.length; i++) {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:',productId, 'Action:', action)
+
+        console.log('USER:',user)
+        if(user == 'AnonymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data..')
+        }
     })
 }
