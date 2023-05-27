f50eac6e9691b729272b30a7902888ddd99af01f
PerezChris99 <kweeziperez712@gmail.com>
2023-05-27 14:38:27 +0300

    cart items total updating automatically

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,6 +1,6 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i = 0; i < updateBtns.length; i++) {
+for(i = 0; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
--- store/views.py
+++ store/views.py
@@ -8,8 +8,20 @@ from .models import *
 
 
 def store(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
