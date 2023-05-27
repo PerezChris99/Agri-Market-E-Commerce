0d2c4f3b613631d2d4d5cdf83967bba57856ccca
PerezChris99 <kweeziperez712@gmail.com>
2023-05-27 16:47:05 +0300

    no need to hide shipping info

--- store/views.py
+++ store/views.py
@@ -47,7 +47,7 @@ def checkout(request):
         cartItems =order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0} #'shipping':False
         cartItems = order['get_cart_items']
 
     context = {'items':items, 'order':order, 'cartItems':cartItems}
